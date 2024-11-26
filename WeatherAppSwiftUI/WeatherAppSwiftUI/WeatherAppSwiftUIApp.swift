907056fb6731071a62395d91672a95db2113c732
sandro <gelashvilialeksandre@gmail.com>
2024-11-26 23:35:53 +0400

    first commit

---
+++ WeatherAppSwiftUI/WeatherAppSwiftUI/WeatherAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppSwiftUIApp.swift
+//  WeatherAppSwiftUI
+//
+//  Created by Sandro Gelashvili on 26.11.24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
